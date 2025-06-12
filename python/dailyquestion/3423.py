5642e330fd4729a50dc003151c4f0ef120549215
YYZ <yyz101306@gmail.com>
2025-06-12 15:28:43 -0400

    added a comment to today's daily question submission

--- python/dailyquestion/3423.py
+++ python/dailyquestion/3423.py
@@ -3,5 +3,6 @@ from typing import List
 class Solution:
     def maxAdjacentDistance(self, nums: List[int]) -> int:
         return max(abs(nums[i]-nums[i+1]) for i in range(-1,len(nums)-1))
+        # for some reason nums[i]-nums[i+1] runs faster than nums[i]-numsi[i-1]
 
         
